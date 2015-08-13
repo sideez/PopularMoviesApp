d20fba82bb5d0e0d27ab5b26a5ac008460512bfc
Faisal Ali <siddee.faisalali@gmail.com>
2015-08-13 11:14:12 -0400

    added movie data model and copyright

---
+++ app/src/main/java/com/sideez/popularmoviesapp/Movies.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2015. Sideez Inc.
+ */
+package com.sideez.popularmoviesapp;
+
+/**
+ * Created by sideez on 2015-08-13.
+ */
+public class Movies {
+    private String mTitle;
+    private String mOverview;
+    private String mPoster;
+    private double mRating;
+    private String mReleaseDate;
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getOverview() {
+        return mOverview;
+    }
+
+    public void setOverview(String overview) {
+        mOverview = overview;
+    }
+
+    public String getPoster() {
+        return mPoster;
+    }
+
+    public void setPoster(String poster) {
+        mPoster = poster;
+    }
+
+    public double getRating() {
+        return mRating;
+    }
+
+    public void setRating(double rating) {
+        mRating = rating;
+    }
+
+    public String getReleaseDate() {
+        return mReleaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        mReleaseDate = releaseDate;
+    }
+}
