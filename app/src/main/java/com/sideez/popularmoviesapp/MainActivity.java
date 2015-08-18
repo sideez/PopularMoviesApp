5b42e3a503febbcde9ad3c95354470b6a161ad82
Faisal Ali <siddee.faisalali@gmail.com>
2015-08-18 13:08:40 -0400

    getting json data using okhttp

--- app/src/main/java/com/sideez/popularmoviesapp/MainActivity.java
+++ app/src/main/java/com/sideez/popularmoviesapp/MainActivity.java
@@ -1,16 +1,89 @@
 package com.sideez.popularmoviesapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Movies mMovies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String order = "popularity.desc";
+
+        final String BASE_URL = "http://api.themoviedb.org/3/discover/movie?";
+        final String QUERY_PARAM = "sort_by";
+        final String API_QUERY_PARAM = "api_key";
+        final String API_KEY = getString(R.string.api_key);
+
+        String movieDBURL = BASE_URL + QUERY_PARAM + "=" + order + "&" + API_QUERY_PARAM + "=" + API_KEY;
+
+        Log.i(TAG, "URL: " + movieDBURL);
+
+        if (isNetworkAvailable()) {
+
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder().url(movieDBURL).build();
+            Call call = client.newCall(request);
+
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Response response) throws IOException {
+
+                    try {
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
+                    } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+
+                }
+            });
+
+        }
+
+    }
+
+    private boolean isNetworkAvailable() {
+
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        boolean isAvailable = false;
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        } else {
+            Toast.makeText(this, "Network unavailable!", Toast.LENGTH_LONG).show();
+        }
+
+        return isAvailable;
+
     }
 
     @Override
