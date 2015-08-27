96fc006769f4f19457b925d6611bcc41480a98bc
Faisal Ali <siddee.faisalali@gmail.com>
2015-08-27 19:27:39 -0400

    fix: code review: When a user clicks the Up button defined in the Action Bar, the Main Activity is recreated.

--- app/src/main/java/com/sideez/popularmoviesapp/ui/MovieDetailsActivity.java
+++ app/src/main/java/com/sideez/popularmoviesapp/ui/MovieDetailsActivity.java
@@ -64,6 +64,9 @@ public class MovieDetailsActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
